5083d154e2e11ff110ca9b486000f60cb9874e27
Lama ALi <lama2015678@gmail.com>
2025-11-01 00:33:34 +0200

    Setup shadcn in the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <div className="text-2xl font-semibold text-blue-600">
-      Welcome to my app
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
     </div>
   );
 }
